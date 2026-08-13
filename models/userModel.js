b5fbc448589a2f446c974ccb3d149616339bc2ef
Samin Ahmed <saminahmed1001@gmail.com>
2026-08-13 21:56:08 +0600

    feat: Add function to insert new user in the database

--- models/userModel.js
+++ models/userModel.js
@@ -6,4 +6,16 @@ export async function getUserDB(userID) {
 
   return user;
 }
+
+export async function createUserDB(data) {
+  try {
+    const user = await prisma.user.create({ data });
+
+    return { user };
+  } catch (error) {
+    const errorCode = error.code;
+
+    return new Error(errorCode);
+  }
+}
 //#endregion
