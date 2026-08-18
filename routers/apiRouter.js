720570ee2fce181f1ecf4c615baad85961584157
Samin Ahmed <saminahmed1001@gmail.com>
2026-08-19 01:58:57 +0600

    feat(auth): Add auth api router

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -4,7 +4,9 @@ export const apiRouter = Router();
 import { userRouter } from "./userRouter.js";
 import { postRouter } from "./postRouter.js";
 import { commentRouter } from "./commentRouter.js";
+import { authRouter } from "./authRouter.js";
 
+apiRouter.use("/auth", authRouter);
 apiRouter.use("/user", userRouter);
 apiRouter.use("/post", postRouter);
 apiRouter.use("/comment", commentRouter);
